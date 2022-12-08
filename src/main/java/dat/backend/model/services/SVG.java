2b7165d0f6f82f8879a7d1fb2035ef3ef9aa5808
Misfosster <williamfossnielsen@hotmail.com>
2022-12-08 11:17:41 +0100

    Dash array lines fixed
    Den kan bytte om på x1 og x2, men ikke y1 og y2 (???????)

--- src/main/java/dat/backend/model/services/SVG.java
+++ src/main/java/dat/backend/model/services/SVG.java
@@ -18,12 +18,11 @@ public class SVG {
             "            <path d=\"M0,0 L12,6 L0,12 L0,0 \" style=\"fill: #000000;\" />\n" +
             "        </marker>\n" +
             "    </defs>";
-    private final static String DASHARRAYTEMPLATE = "<g fill=\"none\" stroke=\"black\" stroke-width=\"4\">\n"+
-            "<line stroke-dasharray=\"5,5\" x1=\"%d\" x2=\"%d\" y1==\"%d\" y2=\"%d\" />\n"+
-            "<line stroke-dasharray=\"5,5\" x1=\"%d\" x2=\"%d\" y1==\"%d\" y2=\"%d\" />";
+    private final static String DASHARRAYTEMPLATE =
+            "<line fill=\"none\" stroke=\"black\" stroke-width=\"4\" stroke-dasharray=\"5,5\" x1=\"%d\" x2=\"%d\" y1==\"%d\" y2=\"%d\" />\n";
 
     public SVG(int x, int y, int height, int width, String viewbox) {
-        svgString.append(String.format(HEADERTEMPLATE, x,y, height, width,viewbox));
+        svgString.append(String.format(HEADERTEMPLATE, x,y, height, width, viewbox));
         svgString.append(DASHARRAYTEMPLATE);
         svgString.append(ARROWHEADSTEMPLATE);
         this.x=x;
@@ -37,10 +36,10 @@ public class SVG {
     svgString.append(String.format(RECTTEMPLATE,x,y,height,width));
     }
 
-    public void addDashArrayLine(int x1, int x2, int y1, int y2)
+    public void addDashArrayLines(int x1, int x2, int y1, int y2)
     {
-        int placeholder=0;
-        svgString.append(String.format(DASHARRAYTEMPLATE,x1,x2,y1,y2,x1,x2,y1-placeholder,y2-placeholder));
+        svgString.append(String.format(DASHARRAYTEMPLATE,x1,x2,y1,y2));
+        svgString.append(String.format(DASHARRAYTEMPLATE,x2,x1,y1,y2));
     }
 
     public void addLine(int x1, int y1, int x2, int y2){
@@ -53,5 +52,21 @@ public class SVG {
     @Override
     public String toString() {
         return svgString + "</svg>";
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
     }
 }
