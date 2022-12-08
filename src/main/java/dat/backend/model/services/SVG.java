fdcc745aefedfce4199bd143802e6ae2d91ce910
Misfosster <williamfossnielsen@hotmail.com>
2022-12-08 10:01:16 +0100

    DASHARRAYTEMPLATE

--- src/main/java/dat/backend/model/services/SVG.java
+++ src/main/java/dat/backend/model/services/SVG.java
@@ -18,9 +18,12 @@ public class SVG {
             "            <path d=\"M0,0 L12,6 L0,12 L0,0 \" style=\"fill: #000000;\" />\n" +
             "        </marker>\n" +
             "    </defs>";
+    private final static String DASHARRAYTEMPLATE = "<g fill=\"none\" stroke=\"black\" stroke-width=\"4\">\n"+
+            "<line stroke-dasharray=\"5,5\" x1=\"%d\" x2=\"%d\" y1==\"%d\" y2=\"%d\" />";
 
     public SVG(int x, int y, int height, int width, String viewbox) {
         svgString.append(String.format(HEADERTEMPLATE, x,y, height, width,viewbox));
+        svgString.append(DASHARRAYTEMPLATE);
         svgString.append(ARROWHEADSTEMPLATE);
         this.x=x;
         this.y=y;
@@ -31,6 +34,11 @@ public class SVG {
         //<rect x="55" y="0" height="600" width="5"
     public void addRect(int x, int y, double height, double width){
     svgString.append(String.format(RECTTEMPLATE,x,y,height,width));
+    }
+
+    public void addDashArrayLine(int x1, int x2, int y1, int y2)
+    {
+        svgString.append(String.format(DASHARRAYTEMPLATE,x1,x2,y1,y2));
     }
 
     public void addLine(int x1, int y1, int x2, int y2){
