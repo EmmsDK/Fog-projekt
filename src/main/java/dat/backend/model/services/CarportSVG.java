2b79d7f3a91d58359a2a644832986d474c71ace8
Misfosster <williamfossnielsen@hotmail.com>
2022-12-08 11:17:41 +0100

    DASHARRAYTEMPLATE fixed
    Den kan bytte om på x1 og x2, men ikke y1 og y2 (???????)

--- src/main/java/dat/backend/model/services/CarportSVG.java
+++ src/main/java/dat/backend/model/services/CarportSVG.java
@@ -1,20 +1,29 @@
 package dat.backend.model.services;
 
 public class CarportSVG {
-    public static SVG createNewSVG(int x, int y, int height, int width, String viewbox){
-    return new SVG(x,y,height,width,viewbox);
+    public static SVG createNewSVG(int x, int y, int height, int width, String viewbox) {
+        return new SVG(x, y, height, width, viewbox);
     }
 
-public static SVG addBeams(SVG svg){
+    public static SVG addBeams(SVG svg) {
 
-    for(int i=0;i< 800;i+=50) {
-        svg.addRect(i,0,600.5,4.5);
+        for (int i = 0; i < 800; i += 50) {
+            svg.addRect(i, 0, 600.5, 4.5);
 
-    }
+        }
         return svg;
 
-}
+    }
 
+    //find på bedre navn
+    /*
+    public static SVG addDashArrayLines(SVG svg, int viewboxWidth,int viewboxHeight) {
+        svg.addDashArrayLine(0, viewboxWidth, 0, viewboxHeight);
+        svg.addDashArrayLine(0, viewboxWidth, viewboxHeight, 0);
+        return svg;
+    }
+
+     */
 
 
 }
