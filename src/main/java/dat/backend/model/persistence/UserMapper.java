905e5fb7cef102d8d565d3a5892861a7125eb3ce
Emilio Lagunas <Cph-ec100@cphbusiness.dk>
2022-12-09 12:46:05 +0100

    Fixed user so I can login to the website again

--- src/main/java/dat/backend/model/persistence/UserMapper.java
+++ src/main/java/dat/backend/model/persistence/UserMapper.java
@@ -38,16 +38,15 @@ class UserMapper {
     static User createUser(String username, String password, String role, ConnectionPool connectionPool) throws DatabaseException {
         Logger.getLogger("web").log(Level.INFO, "");
         User createUser;
-        String sql = "insert into User (username, password, role, balance) values (?,?,?,?)";
+        String sql = "insert into User (username, password, role) values (?,?,?,?)";
         try (Connection connection = connectionPool.getConnection()) {
             try (PreparedStatement ps = connection.prepareStatement(sql)) {
                 ps.setString(1, username);
                 ps.setString(2, password);
                 ps.setString(3, "User");
-                ps.setInt(4, 0);
                 int rowsAffected = ps.executeUpdate();
                 if (rowsAffected == 1) {
-                    user = new User(username, password);
+                    createUser = new User(username, password);
                 } else {
                     throw new DatabaseException("The user with username = " + username + " could not be inserted into the database");
                 }
@@ -56,7 +55,7 @@ class UserMapper {
         catch (SQLException ex) {
             throw new DatabaseException(ex, "Could not insert username into database");
         }
-        return user;
+        return createUser;
     }
 
     public static List<User> getUsers(ConnectionPool connectionPool) {
