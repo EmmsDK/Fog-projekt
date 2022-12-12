eb12b1971cb37b39a762743921dda215ef0be439
cph-jm390 <90380942+cph-jm390@users.noreply.github.com>
2022-12-12 11:41:03 +0100

    billofmats almost done, staticmat load updated

--- src/main/java/dat/backend/model/entities/BillOfMaterials.java
+++ src/main/java/dat/backend/model/entities/BillOfMaterials.java
@@ -1,14 +1,56 @@
 package dat.backend.model.entities;
 
+import dat.backend.model.persistence.BuildingMaterialFacade;
+import dat.backend.model.persistence.BuildingMaterialMapper;
+import dat.backend.model.persistence.ConnectionPool;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BillOfMaterials {
+    ConnectionPool connectionPool = new ConnectionPool();
     List<BuildingMaterial> materialList = new ArrayList<>();
 
     List<Integer> quantity = new ArrayList<>();
     //
 
-    publicStatic
 
+    public BillOfMaterials(List<BuildingMaterial> materialList, List<Integer> quantity) {
+        this.materialList = materialList;
+        this.quantity = quantity;
+    }
+
+    public List<BuildingMaterial> getMaterialList() {
+        return materialList;
+    }
+
+    public void setMaterialList(List<BuildingMaterial> materialList) {
+        this.materialList = materialList;
+    }
+
+    public List<Integer> getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(List<Integer> quantity) {
+        this.quantity = quantity;
+    }
+
+    public void loadStaticMats(){
+        int i=1;
+        setMaterialList(BuildingMaterialMapper.getStaticMaterials(connectionPool));
+        for (BuildingMaterial material:materialList) {
+            if (i == 3) {
+                i=1;
+            }
+                this.quantity.add(i);
+            i++;
+            }
+    }
+
+    public void loadDynamicMats(List<BuildingMaterial> dynamicList){
+        for(BuildingMaterial material:dynamicList){
+            this.materialList.add(material);
+        }
+    }
 }
