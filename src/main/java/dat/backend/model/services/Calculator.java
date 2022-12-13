4184b0785c292ab8ef6a799e67e99da210fdc929
cph-jm390 <90380942+cph-jm390@users.noreply.github.com>
2022-12-13 13:09:55 +0100

    all beam calcs adds

--- src/main/java/dat/backend/model/services/Calculator.java
+++ src/main/java/dat/backend/model/services/Calculator.java
@@ -3,7 +3,7 @@ package dat.backend.model.services;
 public class Calculator {
 
 
-    public static int calcBeams(int length) {
+    public static int calcDrawingBeams(int length) {
         int beamDist = 0;
         int totalAmountOfBeams = 0;
         for (int i = 1; i < 1000; i++) {
@@ -23,5 +23,31 @@ public class Calculator {
         int beamDist = length / amountOfBeams;
         return beamDist;
         //calcPoles(){}
+    }
+
+    public static int findMyBeams(int width){
+       int material_id=0;
+
+        if (width<=480){
+        material_id=9;
+        }else if(width>480 && width <=600){
+            material_id=10;
+        }else{
+            material_id=9;
+        }
+        return material_id;
+    }
+
+    public static int amountOfBeams(int width, int length){
+        int neededBeams=0;
+        if (width<=480){
+            neededBeams=calcDrawingBeams(length);
+        }else if(width>480 && width <=600){
+            neededBeams=calcDrawingBeams(length);
+        }else{
+            neededBeams=calcDrawingBeams(length)*2;
+        }
+        return neededBeams;
+    }
     }
 }
